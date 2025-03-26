a971b22f0a39b93673d47b712483c438a2f09e41
duadrien <adrien.dulou@ynov.com>
2025-03-26 12:05:50 +0100

    BLEH quiz

--- Projet/Fonctions/Quiz.go
+++ Projet/Fonctions/Quiz.go
@@ -2,10 +2,13 @@ package Fonctions
 
 import (
 	"html/template"
+	db "marketplace/DataBase"
 	"net/http"
 )
 
 type DataQuiz struct {
+	IsConnect bool
+	User      db.DB
 }
 
 func QuizPage(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +24,16 @@ func QuizPage(w http.ResponseWriter, r *http.Request) {
 func QuizGet(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../Front/HTML/quiz.html"))
 
-	data := DataQuiz{}
+	cookie, err := r.Cookie("user_id")
+	if err != nil {
+		http.Redirect(w, r, "/Connexion", http.StatusSeeOther)
+		return
+	}
+
+	data := DataQuiz{
+		IsConnect: IsConnectStock,
+		User:      db.GetUser(cookie.Value),
+	}
 	tmpl.Execute(w, data)
 }
 
