0f9e43590012714af7f035ab362e4bd292ee81d1
cjohann <johann.chiarotto@ynov.com>
2025-03-26 09:49:33 +0100

    tkt

--- Projet/Fonctions/VinsMonde.go
+++ Projet/Fonctions/VinsMonde.go
@@ -1,18 +1,24 @@
 package Fonctions
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
+type VinsPays struct {
+	Pays     string
+	WineList []Wine // Changé pour être une slice de Wine
+}
+
 type DataMonde struct {
+	Vins []VinsPays
 }
 
 func VinsMondePage(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		VinsMondePageGet(w, r)
-
 	case "POST":
 		VinsMondePagePost(w, r)
 	}
@@ -20,8 +26,30 @@ func VinsMondePage(w http.ResponseWriter, r *http.Request) {
 
 func VinsMondePageGet(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../Front/HTML/vinsmonde.html"))
+	var Final []VinsPays
 
-	data := DataPepites{}
+	var pays = [5]string{"France", "US", "Portugal", "Italy", "Spain"}
+
+	for i := 0; i < len(pays); i++ {
+		var Instance VinsPays
+		Instance.Pays = pays[i]
+
+		wines := RecupWineContry(pays[i])
+
+		// Limiter à 3 vins maximum
+		if len(wines) > 3 {
+			Instance.WineList = wines[:3] // Prendre seulement les 3 premiers vins
+		} else {
+			Instance.WineList = wines // Prendre tous les vins s'il y en a moins de 3
+		}
+
+		Final = append(Final, Instance)
+	}
+	fmt.Println(Final)
+
+	data := DataMonde{
+		Vins: Final,
+	}
 	tmpl.Execute(w, data)
 }
 
